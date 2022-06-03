1511be4ad549ae1264a2d73f75216112502bcf02
Alex Amarandei <alex.m.amarandei@gmail.com>
2022-06-03 20:40:17 +0300

    :bulb: Added NatSpec comments on all contracts.

--- ethereum-arbitrage-web-platform/contracts/ERC20EUR.sol
+++ ethereum-arbitrage-web-platform/contracts/ERC20EUR.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title Euro Mock Token
+/// @notice Mocks the Euro Token for demonstrating a guaranteed arbitrage opportunity
 contract ERC20EUR is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("Euro", "EUR") {
-        _mint(msg.sender, initialSupply);
+    /// @param _initialSupply The initial supply of token (in wei-like format)
+    constructor(uint256 _initialSupply) public ERC20("Euro", "EUR") {
+        _mint(msg.sender, _initialSupply);
     }
 }
--- ethereum-arbitrage-web-platform/contracts/ERC20RON.sol
+++ ethereum-arbitrage-web-platform/contracts/ERC20RON.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title Euro Mock Token
+/// @notice Mocks the Euro Token for demonstrating a guaranteed arbitrage opportunity
 contract ERC20RON is ERC20 {
+    /// @param _initialSupply The initial supply of token (in wei-like format)
     constructor(uint256 initialSupply) public ERC20("Leu Romanesc", "RON") {
         _mint(msg.sender, initialSupply);
     }
